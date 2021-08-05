a13fa927bb80591d5b0958149c08028301f112fa
Joseph Felicidario <josephflec@gmail.com>
2021-08-05 16:32:06 -0500

    final changes

--- callback_exercises/myThrottle.js
+++ callback_exercises/myThrottle.js
@@ -1,35 +1,47 @@
 Function.prototype.myThrottle = function(interval){
+  let tooSoon = false;
+
+  const throttle = ()=>{
+    if (!tooSoon) {
+      tooSoon = true;
+      setTimeout(function(){tooSoon = false; }, interval);  
+      this();
+    }
     
+  }
+  return throttle;
 }
 
 class Neuron {
     fire() {
       console.log("Firing!");
-    }
   }
+}
   
-  const neuron = new Neuron();
-  // When we create a new Neuron,
-  // we can call #fire as frequently as we want
+const neuron = new Neuron();
+
+neuron.fire = neuron.fire.myThrottle(1000);
   
-  // The following code will try to #fire the neuron every 10ms. Try it in the console:
-  const interval = setInterval(() => {
-    neuron.fire();
-  }, 10);
+const interval = setInterval(() => {
+  neuron.fire();
+}, 10);
   
-  // You can use clearInterval to stop the firing:
-  clearInterval(interval);
+//   // When we create a new Neuron,
+//   // we can call #fire as frequently as we want
   
-  // Using Function#myThrottle, we should be able to throttle
-  // the #fire function of our neuron so that it can only fire
-  // once every 500ms:
+//   // The following code will try to #fire the neuron every 10ms. Try it in the console:
+  // const interval = setInterval(() => {
+  //   neuron.fire();
+  // }, 10);
   
-  neuron.fire = neuron.fire.myThrottle(500);
+//   // You can use clearInterval to stop the firing:
+  // clearInterval(interval);
   
-  const interval = setInterval(() => {
-    neuron.fire();
-  }, 10);
+//   // Using Function#myThrottle, we should be able to throttle
+//   // the #fire function of our neuron so that it can only fire
+//   // once every 500ms:
   
+
   // This time, if our Function#myThrottle worked correctly,
   // the Neuron#fire function should only be able to execute
   // every 500ms, even though we're still trying to invoke it
@@ -37,12 +49,12 @@ class Neuron {
   
   // If we want this behavior for ALL neurons, we can do the same logic in the constructor:
   
-  class Neuron {
-    constructor() {
-      this.fire = this.fire.myThrottle(500);
-    }
+  // class Neuron {
+  //   constructor() {
+  //     this.fire = this.fire.myThrottle(500);
+  //   }
   
-    fire() {
-      console.log("Firing!");
-    }
-  }
+  //   fire() {
+  //     console.log("Firing!");
+  //   }
+  // }
